91d2beddae0cc5bca60f0f91f3c96cd51f501b04
Sebastian Villanueva <62443504+sebvillanueva9@users.noreply.github.com>
2022-08-22 12:30:02 -0400

    Update bootcamp_inventory_discrepancy.py

--- bootcamp_inventory_discrepancy.py
+++ bootcamp_inventory_discrepancy.py
@@ -3,32 +3,31 @@ import pandas as pd
 
 st.title('Stock / Inventory Discrepancy')
 
-st.header('Presenting the Expected / Counted Databases')
+st.header('Databases')
 
-st.subheader('Expected')
+st.subheader('Expected database')
 df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
 st.dataframe(df_expected)
 
-st.subheader('Counted')
+st.subheader('Counted database')
 df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
 st.dataframe(df_counted)
 
 st.markdown("---")
 
-st.header('Plotting Expected / Counted Products')
+st.header('Plotting')
 
-st.subheader('Expected')
+st.subheader('Expected products')
 df1 = df_expected['Retail_Product_Level1Name'].value_counts()
 st.bar_chart(df1)
 
-st.subheader('Counted (Without duplicates)')
-st.write('Before plotting counted case, its necessary to remove the duplicates.')
+st.subheader('Counted (Without duplicates) products')
+st.write('The duplicates were removed in order to present the plot.')
 df_counted_r = df_counted.drop_duplicates("RFID")
 df2 = df_counted_r['Retail_Product_Level1Name'].value_counts()
 st.bar_chart(df2)
 
-st.write('Unlike the expected case, the counted case has one more bar that corresponds to "LAR-TEXTIL", suggesting that there is a difference between the products in each case.')
+st.write('The counted products case has one more bar that corresponds to "LAR-TEXTIL", suggesting that there is a difference between the products in each case.')
 
-st.markdown("---")
-
+st.markdown("<h3 style='text-align: center;'>. . .</h3>", unsafe_allow_html=True)
 st.caption('by Sebastián Villanueva')
